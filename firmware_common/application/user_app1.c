dc510b1f8e8e392c7cf9d70b8ea29aad805138a2
ebailey24 <emmajoyb19@gmail.com>
2024-09-26 19:49:06 -0600

    commit

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -2,6 +2,8 @@
 @file user_app1.c                                                                
 @brief User's tasks / applications are written here.  This description
 should be replaced by something specific to the task.
+EDIT LINE 1
+
 
 ----------------------------------------------------------------------------------------------------------------------
 To start a new task using this user_app1 as a template:
